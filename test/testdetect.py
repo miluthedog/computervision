2a3c682e702aea5b929573c88deb293e37e85ee1
Pha <anhphamhoang04@gmail.com>
2024-10-14 09:39:00 +0700

    change to brg

--- test/testdetect.py
+++ test/testdetect.py
@@ -10,7 +10,7 @@ while True:
     color = cv2.cvtColor(info, cv2.COLOR_BGR2GRAY)
     face = trainedData.detectMultiScale(color, 1.1, 4)
     for (x, y, w, h) in face:
-        cv2.rectangle(info, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        cv2.rectangle(info, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
     cv2.imshow('image', info)
 
